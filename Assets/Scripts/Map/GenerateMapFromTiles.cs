b10ae42f87cfc0f28c062d6266d55b2df0853b54
Colan Biemer <colan@comcast.net>
2020-03-03 20:28:08 -0500

    level generation and tile map can almost be converted to a file that can be read

---
+++ Assets/Scripts/Map/GenerateMapFromTiles.cs
@@ -0,0 +1,44 @@
+﻿#if UNITY_EDITOR
+using UnityEngine.Assertions;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GenerateMapFromTiles : MonoBehaviour
+{
+    private Tilemap tilemap;
+    void Start()
+    {
+        tilemap = GetComponent<Tilemap>();
+        Assert.IsNotNull(tilemap);
+
+
+        int xMin = tilemap.cellBounds.xMin;
+        int xMax = tilemap.cellBounds.xMax;
+        int yMin = tilemap.cellBounds.yMin;
+        int yMax = tilemap.cellBounds.yMax;
+
+        string test = "";
+
+        for (int y = yMax; y >= yMin; --y)
+        {
+            for (int x = xMin; x < xMax; ++x)
+            {
+                TileBase tile = tilemap.GetTile(new Vector3Int(x, y, 0));
+                if (tile == null)
+                {
+                    test += " ";
+                }
+                else
+                {
+                    test += TileExtensions.ToTile(tile.name).ToCharacter();
+                }
+            }
+
+            test += "\n";
+        }
+
+        Debug.Log(test);
+    }
+}
+
+#endif
---
+++ Assets/Scripts/Map/Tile.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+public enum Tile
+{
+    block,
+    crate,
+    playerOneStart,
+    playerOneFinish,
+    basicEnemy
+}
+
+public static class TileExtensions
+{
+    public static Tile ToTile(string tileName)
+    {
+        switch (tileName)
+        {
+            case "Crate":
+                return Tile.crate;
+            case "Blocks_0":
+                return Tile.block;
+            case "Blocks_7":
+                return Tile.playerOneStart;
+            case "Blocks_8":
+                return Tile.playerOneFinish;
+            default:
+                throw new Exception($"{tileName} not found");
+        }
+    }
+
+    public static char ToCharacter(this Tile tile)
+    {
+        switch (tile)
+        {
+            case Tile.block:
+                return 'b';
+            case Tile.crate:
+                return 'c';
+            case Tile.playerOneStart:
+                return 's';
+            case Tile.playerOneFinish:
+                return 'f';
+            case Tile.basicEnemy:
+                return 'B';
+            default:
+                throw new Exception($"{tile} does not have valid to character entry");
+        }
+    }
+}
