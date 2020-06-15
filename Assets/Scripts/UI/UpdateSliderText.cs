9c39baaea10db0c5e486e0f38148bd565549be1b
Colan Biemer <colan@comcast.net>
2020-06-15 16:01:13 -0400

    update slider text implemented and the start of configuration in the game is going

---
+++ Assets/Scripts/UI/UpdateSliderText.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine.Assertions;
+using UnityEngine.UI;
+using UnityEngine;
+using TMPro;
+
+public class UpdateSliderText : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI text = null;
+
+    [SerializeField]
+    private string start = "";
+
+    private Slider slider = null;
+
+    private void Awake()
+    {
+        slider = GetComponent<Slider>();
+
+        Assert.IsNotNull(slider);
+        Assert.IsNotNull(text);
+    }
+
+    private void Start()
+    {
+        slider.onValueChanged.AddListener((float val) =>
+        {
+            if (slider.wholeNumbers)
+            {
+                text.text = $"{start}{(int) val}";
+            }
+            else
+            { 
+                text.text = $"{start}{val}";
+            }
+        });
+    }
+}
