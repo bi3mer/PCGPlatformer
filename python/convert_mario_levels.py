b59de597b0570f3c68bfc422dc30ab497105842c
bi3mer <colan@comcast.net>
2020-06-15 13:09:45 -0400

    new levels

--- python/convert_mario_levels.py
+++ python/convert_mario_levels.py
@@ -5,6 +5,7 @@ import os
 def convert_character(character):
     if character == '-': return ''
     elif character == 'X': return 'b'
+    elif character == 'x': return 'b'
     elif character == 'S': return 'b'
     elif character == 'Q': return 'b'
     elif character == 'E': return 'A'
@@ -17,39 +18,45 @@ def convert_character(character):
     elif character == 'b': return 'b'
     elif character == '?': return 'b'
     elif character == 'f': return 'f'
+    elif character == '#': return 'b'
+    elif character == 'p': return 'b'
+    elif character == 'P': return 'b'
+    elif character == 'e': return 'A'
+    elif character == 'c': return 'M'
+    elif character == 'g': return 'A'
     else:
         print(f'{character} not handled by conversion')
         return character
 
-def main():
-    mario_levels_path = os.path.join(Path.home(), 'data', 'vglc', 'Super Mario Bros', 'Processed')
-    pcg_platformer_path = os.path.join('..', 'Assets', 'Resources', 'Levels', 'SuperMarioBros')
-    local = os.path.join('.', 'regular_vglc')
+def convert(path, name, start_row, start_col, vertical=False):
+    pcg_platformer_path = os.path.join('..', 'Assets', 'Resources', 'Levels', name)
 
-    for file_name in os.listdir(mario_levels_path):
-        f = open(os.path.join(mario_levels_path, file_name))
+    if not os.path.exists(pcg_platformer_path):
+        os.mkdir(pcg_platformer_path)
+
+    for file_name in os.listdir(path):
+        f = open(os.path.join(path, file_name))
         content = f.readlines()
         f.close()
 
-        unconverted = []
         converted = []
         for line in content:
             row = list(line.strip())
             row.append('f')
-            unconverted.append(row)
             converted.append([convert_character(c) for c in row])
 
         # where the player starts
-        unconverted[-2][3] = 's' 
-        converted[-2][3] = 's'
+        converted[start_row][start_col] = 's' 
 
         f = open(os.path.join(pcg_platformer_path, file_name), 'w')
         f.write(json.dumps(converted))
         f.close()
-
-        f = open(os.path.join(local, file_name), 'w')
-        f.write(json.dumps(unconverted))
-        f.close()
+        
+def get_vglc_path(game_name):
+    return os.path.join(Path.home(), 'data', 'TheVGLC', game_name, 'Processed')
 
 if __name__ == '__main__':
-    main()
+    convert(get_vglc_path("Super Mario Bros"), "SuperMarioBros", -2, 3)
+    convert(get_vglc_path("Super Mario Bros 2 (Japan)"), "SuperMarioBros2Japan", -2, 3)
+    convert(get_vglc_path("Super Mario Land"), "SuperMarioLand", -2, 3)
+    convert(os.path.join(get_vglc_path("Super Mario Bros 2"), 'WithEnemies'), "SuperMarioBros2", -2, 3)
